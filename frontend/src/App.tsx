86d2593aa84e9a9a4b6c498084a72f2fa2c934a7
Harnoor Singh Aulakh <harnooraulakh16@gmail.com>
2025-01-15 19:28:20 +0530

    initial commit

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,7 @@
 const App = () => {
   return (
     <div className="h-screen w-screen flex justify-center items-center">
-      <img className="absolute left-0 top-0 w-full -z-1" src="bg.jpg" />
-      <img className="h-full z-0" src="horny.png" alt="HornyWedsNeeraj" />
+      <h1>Hello World</h1>
     </div>
   );
 };
