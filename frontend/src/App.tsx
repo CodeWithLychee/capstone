479a35ac55175c92803a416e1ef6f89845ba81db
Harnoor Singh Aulakh <harnooraulakh16@gmail.com>
2025-01-15 21:46:48 +0530

    initial commit

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 const App = () => {
   return (
     <div className="h-screen w-screen flex justify-center items-center">
-      <h1>Hello Thapar</h1>
+      <h1>Deployment Successfull</h1>
     </div>
   );
 };
