2ed6f0e0386105bbb1f2b4857b4db8c2105bc492
rbhagat32 <raghavbhagat32@gmail.com>
2025-01-15 16:08:48 +0530

    init

---
+++ frontend/src/App.tsx
@@ -0,0 +1,10 @@
+const App = () => {
+  return (
+    <div className="h-screen w-screen flex justify-center items-center">
+      <img className="absolute left-0 top-0 w-full -z-1" src="bg.jpg" />
+      <img className="h-full z-0" src="horny.png" />
+    </div>
+  );
+};
+
+export default App;
