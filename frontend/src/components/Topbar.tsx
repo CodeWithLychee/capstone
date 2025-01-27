cc61be5553ae12329c3a1005d837ca5dfed4df6e
Harnoor Singh Aulakh <harnooraulakh16@gmail.com>
2025-01-27 13:10:13 +0530

    bug fixed

--- frontend/src/components/Topbar.tsx
+++ frontend/src/components/Topbar.tsx
@@ -2,8 +2,8 @@ import { MdKeyboardArrowDown } from "react-icons/md";
 
 export default function Topbar() {
   return (
-    <div className="bg-[#ededed] h-[4.5rem] w-full shadow-md flex items-center">
-      <div className="flex flex-row items-center p-2 pr-5 gap-4 absolute right-0">
+    <div className="bg-[#ededed] h-[4rem] w-full shadow-md flex items-center">
+      <div className="flex flex-row items-center py-1 px-2 pr-5 gap-4 absolute right-0">
         <div className="bg-[#7C7B7D] w-[2px] h-[2rem]"></div>
         <img src="/dp.png" alt="" className="rounded-full w-[3rem] h-[3rem]"/>
         <div>
